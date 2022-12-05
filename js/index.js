4740bf0c01268a7f2177183b4c0ba1923f3f4556
mihail <mikhailvodolagin@icloud.com>
2022-12-05 18:48:13 +0500

    style

--- js/index.js
+++ js/index.js
@@ -27,24 +27,23 @@ function createWidgets() {
 
 function createWidget(currentWeather) {
     let icon = `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`;
-    let map = `https://maps.geoapify.com/v1/staticmap?style=osm-bright-smooth&width=400&height=400&center=lonlat:${currentWeather.coord.lon},${currentWeather.coord.lat}&zoom=15&marker=lonlat:${currentWeather.coord.lon},${currentWeather.coord.lat};color:%23ff0000;size:medium&apiKey=98645432a8444ff996d0cd1f96bed12e`;
+    let map = `https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=400&height=400&center=lonlat:${currentWeather.coord.lon},${currentWeather.coord.lat}&zoom=10&marker=lonlat:${currentWeather.coord.lon},${currentWeather.coord.lat};color:%23ff0000;size:medium&apiKey=7b128cd8393743309d239f6e08f5fa23`;
     let widget = document.createElement("li");
     widget.classList.add("widgets__widget");
     widget.innerHTML = `
         <div class="widgets__title">
             <h1 class="widgets__name">${currentWeather.name}</h1>
-            <h2>${currentWeather.main.temp.toFixed(0)}°</h2>
+            <h2>${Math.floor(currentWeather.main.temp)}°</h2>
             <img src=${icon} alt="Иконка">
             <h2 class="widgets__description">${currentWeather.weather[0].description}</h2>
         </div>
         <div class="widgets__info">
-            <p class="widgets__item">Ощущается как: ${currentWeather.main.feels_like.toFixed(0)}°</p>
             <p class="widgets__item">Скорость ветра: ${currentWeather.wind.speed.toFixed(0)} м/c</p>
             <p class="widgets__item">Влажность: ${currentWeather.main.humidity} %</p>
             <p class="widgets__item">Давление: ${currentWeather.main.pressure} ГП</p>
         </div>
         <div class="widgets__map-wrapper">
-            <a href="https://maps.yandex.ru/?ll=${currentWeather.coord.lon},${currentWeather.coord.lat}&z=10"><img class="widgets__map" src=${map} alt="Интерактивная Карта"></a>
+            <a href="https://maps.yandex.ru/?ll=${currentWeather.coord.lon},${currentWeather.coord.lat}&z=10" target="_blank"><img class="widgets__map" src=${map} alt="Карта"></a>
         </div>`;
     list.appendChild(widget);
     localStorage.setItem("list", JSON.stringify(list.innerHTML));
